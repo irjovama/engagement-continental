791fbfcc36f0398dcb49af545b370ca8410bee34
IRVING JONES VALDES MACIEL <irjovama@gmail.com>
2023-11-08 16:49:07 -0600

    fix entity user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -30,6 +30,36 @@ export class User {
   @Column()
   modality: string;
 
+  @Column()
+  unidadDeNegocio: string;
+
+  @Column()
+  areaDeTrabajo: string;
+
+  @Column()
+  subAreaDeTrabajo: string;
+
+  @Column()
+  ubicacionFisica: string;
+
+  @Column()
+  local: string;
+
+  @Column()
+  naturalezaDelPuesto: string;
+
+  @Column()
+  grupoOcupacionalNivel2: string;
+
+  @Column()
+  anosEnLaOrganizacion: string;
+
+  @Column()
+  edad: string;
+
+  @Column()
+  generacion: string;
+
   @DeleteDateColumn()
   deleteAt: Date;
 }
