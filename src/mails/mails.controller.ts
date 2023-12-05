51bd3eeae2b952fe6dc3ddbec44600e07a39499f
IRVING JONES VALDES MACIEL <irjovama@gmail.com>
2023-12-05 10:31:37 -0600

    final message

--- src/mails/mails.controller.ts
+++ src/mails/mails.controller.ts
@@ -18,17 +18,27 @@ export class MailsController {
     @Body('email') destinatario: string,
     @Body('token') token: string,
   ) {
-    const asunto = `¿Eres parte del 10% que aún no contesta la encuesta? 😯`;
+    const asunto = `Último día para contestar la encuesta 😯`;
     const mensaje = `<p>Buenos días a todos y todas,</p>
-<p>En Continental valoramos la opinión de nuestros colaboradores y nos esforzamos por crear un entorno de trabajo positivo y centrado en las personas. Para lograrlo, es fundamental entender tus necesidades, expectativas y sugerencias.</p>
-<p>Por esta razón, te invitamos a realizar la <strong>Encuesta de Engagement  y Clima 2023-2</strong>, la cual nos ayudará a medir el nivel de compromiso y clima de nuestros colaboradores en relación con su trabajo y con la organización. Tus respuestas son completamente anónimas y confidenciales, por lo que te animamos a ser lo más honesto posible.</p>    
-<p>Para contestar la encuesta, en el siguiente enlace: <a href='https://continental-engagement-front.vercel.app/?token=${token}'>Contestar encuesta</a></p>    
-<p>Tu participación es fundamental para ayudarnos a identificar áreas de mejora y seguir fortaleciendo nuestro compromiso con el bienestar en la organización.</p> 
-<p>Recuerda que tienes hasta HOY para responder.</p>
-<p>Si tienes alguna pregunta o experimentas algún problema técnico al acceder a la encuesta, no dudes en contactar a bienestarytalento@continental.edu.pe</p>
-<p>Un abrazo,</p>
-<p>Bienestar y Talento.</p>
-    `;
+    <p>En Continental valoramos la opinión de nuestros colaboradores y nos esforzamos por crear un entorno de trabajo positivo y centrado en las personas. Para lograrlo, es fundamental entender tus necesidades, expectativas y sugerencias.    </p>
+   <p>Por esta razón, te invitamos a realizar la <strong>Encuesta de Engagement  y Clima 2023-2</strong>, la cual nos ayudará a medir el nivel de compromiso y clima de nuestros colaboradores en relación con su trabajo y con la organización. Tus respuestas son completamente <strong>anónimas y confidenciales</strong>, por lo que te animamos a ser lo más honesto posible.    </p> 
+   <p>Para contestar la encuesta, en el siguiente enlace:  <a href='https://continental-engagement-front.vercel.app/?token=${token}'>Contestar encuesta</a> </p>
+   <p>Tu participación es fundamental para ayudarnos a identificar áreas de mejora y seguir fortaleciendo nuestro compromiso con el bienestar en la organización.   </p>
+   <p><strong>Recuerda que tienes hasta HOY para responder.   </strong></p>
+   <p>Si tienes alguna pregunta o experimentas algún problema técnico al acceder a la encuesta, no dudes en contactar a bienestarytalento@continental.edu.pe   </p>
+   <p>Un abrazo,   </p>
+   <p>Bienestar y Talento.   </p>`;
+    //     const asunto = `¿Eres parte del 10% que aún no contesta la encuesta? 😯`;
+    //     const mensaje = `<p>Buenos días a todos y todas,</p>
+    // <p>En Continental valoramos la opinión de nuestros colaboradores y nos esforzamos por crear un entorno de trabajo positivo y centrado en las personas. Para lograrlo, es fundamental entender tus necesidades, expectativas y sugerencias.</p>
+    // <p>Por esta razón, te invitamos a realizar la <strong>Encuesta de Engagement  y Clima 2023-2</strong>, la cual nos ayudará a medir el nivel de compromiso y clima de nuestros colaboradores en relación con su trabajo y con la organización. Tus respuestas son completamente anónimas y confidenciales, por lo que te animamos a ser lo más honesto posible.</p>
+    // <p>Para contestar la encuesta, en el siguiente enlace: <a href='https://continental-engagement-front.vercel.app/?token=${token}'>Contestar encuesta</a></p>
+    // <p>Tu participación es fundamental para ayudarnos a identificar áreas de mejora y seguir fortaleciendo nuestro compromiso con el bienestar en la organización.</p>
+    // <p>Recuerda que tienes hasta HOY para responder.</p>
+    // <p>Si tienes alguna pregunta o experimentas algún problema técnico al acceder a la encuesta, no dudes en contactar a bienestarytalento@continental.edu.pe</p>
+    // <p>Un abrazo,</p>
+    // <p>Bienestar y Talento.</p>
+    //     `;
     //     const asunto = `¿Y la encuesta de Engagement y Clima? 🥲`;
     //     const mensaje = `<p>Buenos días a todos y todas,</p>
     // <p>En Continental valoramos la opinión de nuestros colaboradores y nos esforzamos por crear un entorno de trabajo positivo y centrado en las personas.
